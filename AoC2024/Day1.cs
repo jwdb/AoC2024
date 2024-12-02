252d3313be392bbdf466d32676615c20e6d6514f
Jan-Willem de Bruyn <jan.willem.de.bruyn@avivasolutions.nl>
2024-12-02 08:00:10 +0100

    Added Day2

--- AoC2024/Day1.cs
+++ AoC2024/Day1.cs
@@ -1,33 +1,39 @@
-﻿var input = File.ReadAllText("day1.txt");
-
-var lines = input.Split(Environment.NewLine, StringSplitOptions.RemoveEmptyEntries)
-    .Select(line => line.Split(" ", StringSplitOptions.RemoveEmptyEntries))
-    .ToArray();
-    
-var left = lines.Select(line  => int.Parse(line[0])).ToList();
-var right = lines.Select(line => int.Parse(line[1])).ToList();
-var answer = 0;
-Console.WriteLine($"Day 1: {string.Join(" ", left)}");
-while (left.Count > 0 && right.Count > 0)
+﻿public class Day1
 {
-    var leftMin = left.Min();
-    var rightMin = right.Min();
-    var difference = Math.Abs(leftMin - rightMin);
-    answer += difference;
-    left.Remove(leftMin);
-    right.Remove(rightMin);
+    public static void Main(params string[] args)
+    {
+        var input = File.ReadAllText("day1.txt");
+
+        var lines = input.Split(Environment.NewLine, StringSplitOptions.RemoveEmptyEntries)
+            .Select(line => line.Split(" ", StringSplitOptions.RemoveEmptyEntries))
+            .ToArray();
+
+        var left = lines.Select(line => int.Parse(line[0])).ToList();
+        var right = lines.Select(line => int.Parse(line[1])).ToList();
+        var answer = 0;
+        Console.WriteLine($"Day 1: {string.Join(" ", left)}");
+        while (left.Count > 0 && right.Count > 0)
+        {
+            var leftMin = left.Min();
+            var rightMin = right.Min();
+            var difference = Math.Abs(leftMin - rightMin);
+            answer += difference;
+            left.Remove(leftMin);
+            right.Remove(rightMin);
+        }
+
+        Console.WriteLine(answer);
+
+        answer = 0;
+        left = lines.Select(line => int.Parse(line[0])).ToList();
+        right = lines.Select(line => int.Parse(line[1])).ToList();
+
+        foreach (var m in left)
+        {
+            var rightCount = right.Count(c => c == m);
+            answer += m * rightCount;
+        }
+
+        Console.WriteLine(answer);
+    }
 }
-
-Console.WriteLine(answer);
-
-answer = 0;
-left = lines.Select(line  => int.Parse(line[0])).ToList();
-right = lines.Select(line => int.Parse(line[1])).ToList();
-
-foreach (var m in left)
-{
-    var rightCount = right.Count(c => c == m);
-    answer += m * rightCount;
-}
-
-Console.WriteLine(answer);
---
+++ AoC2024/Day2.cs
@@ -0,0 +1,93 @@
+﻿public class Day2
+{
+    public static void Main(params string[] args)
+    {
+        var input = File.ReadAllText("inputs/day2.txt");
+
+        var lines = input.Split(Environment.NewLine, StringSplitOptions.RemoveEmptyEntries)
+            .Select(line => line.Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray())
+            .ToArray();
+
+        var answer = lines.Where(IsSafe).Count();
+
+        Console.WriteLine(answer);
+
+        answer = lines.Where(IsSafeWithProblemDampener).Count();
+
+        Console.WriteLine(answer);
+    }
+
+    private static bool IsSafe(int[] input)
+    {
+        bool isReverse = input[0] > input[1];
+        for (int i = 0; i < input.Length - 1; i++)
+        {
+            var current = input[i];
+            var next = input[i + 1];
+
+            if (Math.Abs(current - next) is < 1 or > 3)
+            {
+                return false;
+            }
+
+            if (current > next != isReverse)
+            {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    private static bool IsSafeWithProblemDampener(int[] input)
+    {
+        var inputList = new List<int>(input);
+        bool isReverse = inputList[0] > inputList[1];
+        for (int i = 0; i < inputList.Count - 1; i++)
+        {
+            var current = inputList[i];
+            var next = inputList[i + 1];
+
+            if (Math.Abs(current - next) is < 1 or > 3)
+            {
+                return ProblemDampener(input, i);
+            }
+
+            if (current > next != isReverse)
+            {
+                return ProblemDampener(input, i);
+            }
+        }
+
+        return true;
+    }
+
+    private static bool ProblemDampener(int[] input, int i)
+    {
+        var newListOne = new List<int>(input);
+        newListOne.RemoveAt(i);
+        if (IsSafe(newListOne.ToArray()))
+        {
+            return true;
+        }
+
+        var newListTwo = new List<int>(input);
+        newListTwo.RemoveAt(i + 1);
+        if (IsSafe(newListTwo.ToArray()))
+        {
+            return true;
+        }
+
+        if (i > 0)
+        {
+            var newListThree = new List<int>(input);
+            newListThree.RemoveAt(i - 1);
+            if (IsSafe(newListThree.ToArray()))
+            {
+                return true;
+            }
+        }
+
+        return false;
+    }
+}
